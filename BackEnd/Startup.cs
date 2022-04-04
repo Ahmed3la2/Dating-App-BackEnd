c20a0809df8a11f60206c7288716a965cd4b39a5
ahmed alaa <ahmedalaa40400@gmail.com>
2022-04-04 13:08:27 +0200

    Add Cors

--- BackEnd/Startup.cs
+++ BackEnd/Startup.cs
@@ -30,6 +30,7 @@ namespace BackEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlServer(_Config.GetConnectionString("defaultconnection"));
@@ -54,6 +55,8 @@ namespace BackEnd
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
